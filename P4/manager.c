554d1dddcbf2283ff4bbb6a09325eccdcdae45b7
newtownfam <newtownfam@gmail.com>
2018-02-19 18:06:17 -0500

    outline

--- P4/manager.c
+++ P4/manager.c
@@ -4,5 +4,47 @@
  * Project4 Manager.c Source Code
  */
 
-// global 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// global guys
 char memory[64]; //memory
+
+/*
+ * "map takes the value and determines if a
+ * to complete said instructions." - grandmaster coache
+ */
+int map (int process, char* instruction, int address, int value)
+{
+	return 0;
+
+}
+
+/*
+ * "store takes the value and stores it into the physical address specified
+ * to complete said instructions." - grandmaster coache
+ * 1 - check valid bit
+ * 2 - check write permission 
+ * 3 - store bit
+ */
+int store (int process, char* instruction, int address, int value)
+{
+	return 0;
+}
+
+
+/*
+ * "load takes the value and stores it into the physical address specified
+ * to complete said instructions." - grandmaster coache
+ */
+int load (int process, char* instruction, int address, int value)
+{
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	return 1;
+}
