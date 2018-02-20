bdfcddc7b24e12781d598346493da4245baf540f
newtownfam <newtownfam@gmail.com>
2018-02-19 22:23:20 -0500

    load

--- P4/manager.c
+++ P4/manager.c
@@ -23,13 +23,14 @@ typedef struct {
 } pageEntry;
 
 
-char memory[64]; //memory
+unsigned char memory[64]; //memory
+pageEntry pageTable[5];
 
 /********************************************************* FUNCTION DECLARATIONS *************************************************************/
 
-void map (int process, char* instruction, int address, int value);
-void store (int process, char* instruction, int address, int value);
-void load (int process, char* instruction, int address, int value);
+int map (int process, char* instruction, int address, int value);
+int store (int process, char* instruction, int address, int value);
+int load (int process, char* instruction, int address, int value);
 void initialize(); // init the page table
 
 /******************************************************** HELPER FUNCTIONS *******************************************************************/
@@ -37,7 +38,7 @@ void initialize(); // init the page table
 void masterFunction (int process, char* instruction, int address, int value) 
 {
 	if (!(strcmp(instruction, "map")))   { map(process, instruction, address, value);   }
-	if (!(strcmp(instruction, "load")))  { load(process, instruction, vaddress, value);  }
+	if (!(strcmp(instruction, "load")))  { load(process, instruction, address, value);  }
 	if (!(strcmp(instruction, "store"))) { store(process, instruction, address, value); }
 	else { printf("ERROR: You Specified an Invalid Instruction"); }
 }
@@ -62,25 +63,27 @@ number using the virtual address. For example, virtual address of 3 corresponds 
 0. value argument represents the write permission for the page. If value=1 then the page is writeable
 and readable. If value=0, then the page is only readable, i.e., all mapped pages are readable. These
 permissions can be modified by using a second map instruction for the target page. */
-void map (int process, char* instruction, int address, int value) { printf("Oh yea... the map function doesnt do shit\n"); } 
+int map (int process, char* instruction, int address, int value) { printf("Oh yea... the map function doesnt do shit\n"); return 1;} 
 
 /* store instructs the memory manager to write the supplied value into the physical memory location
 associated with the provided virtual address, performing translation and page swapping as necessary.
 Note, page swapping is a requirement for part 2 only. */
-void store (int process, char* instruction, int address, int value) { printf("Oh yeas... the store function doesnt do shit\n"); }
+int store (int process, char* instruction, int address, int value) { printf("Oh yeas... the store function doesnt do shit\n"); return 1;}
 
 /* load instructs the memory manager to return the byte stored at the memory location specified by
 virtual address. Like the store instruction, it is the memory manager’s responsibility to translate
 and swap pages as needed. Note, the value parameter is not used for this instruction, but a dummy
 value (e.g., 0) should always be provided. */
-void load (int process, char* instruction, int address, int value) 
+int load (int process, char* instruction, int address, int value) 
 { 
 	if (pageTable[process].validBit != 1) { printf("ERROR: Process [%i] not Found\n", process); return -1; } // check validity
+	
 }
 
 /*********************************************************** MAIN ****************************************************************************/
 int main(int argc, char **argv)
 {
+	pageEntry pageTable;
 	initialize(); // initilialize structs
 	char *userInput[4]; 
  	int process_id;
