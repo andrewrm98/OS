c45af048c3d523dee71bf48c74659de25d278e7e
newtownfam <newtownfam@gmail.com>
2018-02-19 20:56:22 -0500

    stuff

--- P4/manager.c
+++ P4/manager.c
@@ -12,39 +12,54 @@
 // global guys
 char memory[64]; //memory
 
-/*
- * "map takes the value and determines if a
- * to complete said instructions." - grandmaster coache
- */
-int map (int process, char* instruction, int address, int value)
-{
-	return 0;
-
-}
-
-/*
- * "store takes the value and stores it into the physical address specified
- * to complete said instructions." - grandmaster coache
- * 1 - check valid bit
- * 2 - check write permission 
- * 3 - store bit
- */
-int store (int process, char* instruction, int address, int value)
-{
-	return 0;
-}
-
-
-/*
- * "load takes the value and stores it into the physical address specified
- * to complete said instructions." - grandmaster coache
- */
-int load (int process, char* instruction, int address, int value)
-{
-	return 0;
-}
+// functions to make
+int map (int process, char* instruction, int address, int value) { return 0; }
+int store (int process, char* instruction, int address, int value) { return 0; }
+int load (int process, char* instruction, int address, int value) { return 0; }
 
 int main(int argc, char **argv)
 {
+	char *userInput[4]; 
+ 	int process_id;
+	char* instruction_type;
+	int virtual_address;
+	int value;
+
+   	while(!feof(stdin))
+  	{
+		// tokenize first input
+   		char argsArr[4]; //store arguments in array
+    	char * args = argsArr; // make pointer to array
+    	const char s[2] = ","; // token to check for
+    	char *token; // token creation
+
+   		/* tokenize first input */
+    	printf("Instruction? ");
+    	fgets(args,32,stdin); 
+    	token = strtok(args, s);
+    	if (!token) { printf("ERROR: Invalid Input\n"); return 1; }
+    	userInput[0] = token;
+    	printf("userInput[0]: %s\n", token);
+
+
+    	/* tokenize rest of inputs */
+ 		int i = 1;
+    	while(i < 4) 
+    	{
+    		if (!token) { printf("ERROR: Invalid Input\n"); return 1; }
+    		//printf("looping\n");
+    		token = strtok(NULL, s);
+    		userInput[i] = token;
+    		printf("userInput[%i]: %s\n", i, token);
+    		i++;
+    	}
+
+    	/* assign tokenized values */
+    	process_id = atoi(userInput[0]);
+		instruction_type = userInput[1];
+		virtual_address = atoi(userInput[2]);
+		value = atoi(userInput[3]);
+		printf("process_id: %i\n instruction_type: %s\n virtual_address: %i\n value: %i\n", process_id, instruction_type, virtual_address, value);
+  }
 	return 1;
 }
