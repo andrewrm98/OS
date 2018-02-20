f1ec1718b5e262cf7b07c81655d07667e22b873b
newtownfam <newtownfam@gmail.com>
2018-02-19 22:16:21 -0500

    working on load

--- P4/manager.c
+++ P4/manager.c
@@ -1,7 +1,7 @@
 /*
  * Andrew Morrison
  * Peter Christakos
- * Project4 Manager.c Source Code
+ * Project4 manager.c Source Code
  */
 
 #include <stdlib.h>
@@ -9,37 +9,79 @@
 #include <string.h>
 #include <unistd.h>
 
-// global guys
+/********************************************************* STRUCTS/ARRAYS ********************************************************************/
+
+/* struct to assign bit size of each */
+typedef struct {
+    u_int8_t presentBit:1; //1 if present, 0 if on disk
+    u_int8_t validBit:1; //1 if valid
+    u_int8_t value:1; //1 if can be written, 0 if read-only
+    u_int8_t page:3; //page number ie frame
+    u_int8_t processId:4; //PID
+    u_int8_t pageNum:4; //Location in memory
+    u_int8_t instruction:2;
+} pageEntry;
+
+
 char memory[64]; //memory
+
+/********************************************************* FUNCTION DECLARATIONS *************************************************************/
 
 void map (int process, char* instruction, int address, int value);
 void store (int process, char* instruction, int address, int value);
 void load (int process, char* instruction, int address, int value);
+void initialize(); // init the page table
 
-// functions to make
-void map (int process, char* instruction, int address, int value) { printf("Oh yea... the map function doesnt do shit\n"); } 
-void store (int process, char* instruction, int address, int value) { printf("Oh yeas... the store function doesnt do shit\n"); }
-void load (int process, char* instruction, int address, int value) { printf("Oh yea... the load function doesnt do shit\n"); }
-
+/******************************************************** HELPER FUNCTIONS *******************************************************************/
 /* runs selected instruction */
-void masterFunction (int process_id, char* instruction_type, int virtual_address, int value) 
+void masterFunction (int process, char* instruction, int address, int value) 
 {
-	if (!(strcmp(instruction_type, "map"))) 
-	{ 
-		map(process_id, instruction_type, virtual_address, value);
-	}
-	if (!(strcmp(instruction_type, "load"))) 
-	{ 
-		load(process_id, instruction_type, virtual_address, value);
-	}
-	if (!(strcmp(instruction_type, "store"))) 
-	{ 
-		store(process_id, instruction_type, virtual_address, value);
-	}
+	if (!(strcmp(instruction, "map")))   { map(process, instruction, address, value);   }
+	if (!(strcmp(instruction, "load")))  { load(process, instruction, vaddress, value);  }
+	if (!(strcmp(instruction, "store"))) { store(process, instruction, address, value); }
+	else { printf("ERROR: You Specified an Invalid Instruction"); }
 }
 
+void initialize()
+{
+	for (int i = 0; i < 4; i++) {
+	        pageTable[i].validBit = 0;
+	        pageTable[i].presentBit = 0;
+	        pageTable[i].value = 0;
+	        pageTable[i].page = -1;
+	        pageTable[i].pageNum = 0;
+	        pageTable[i].processId = i;
+	    }
+}
+
+/*************************************************** INSTRUCTION FUNCTIONS *******************************************************************/
+
+/* map tells the memory manager to allocate a physical page, i.e., it creates a mapping in the page table
+between a virtual and physical address. The manager must determine the appropriate virtual page
+number using the virtual address. For example, virtual address of 3 corresponds to virtual page
+0. value argument represents the write permission for the page. If value=1 then the page is writeable
+and readable. If value=0, then the page is only readable, i.e., all mapped pages are readable. These
+permissions can be modified by using a second map instruction for the target page. */
+void map (int process, char* instruction, int address, int value) { printf("Oh yea... the map function doesnt do shit\n"); } 
+
+/* store instructs the memory manager to write the supplied value into the physical memory location
+associated with the provided virtual address, performing translation and page swapping as necessary.
+Note, page swapping is a requirement for part 2 only. */
+void store (int process, char* instruction, int address, int value) { printf("Oh yeas... the store function doesnt do shit\n"); }
+
+/* load instructs the memory manager to return the byte stored at the memory location specified by
+virtual address. Like the store instruction, it is the memory manager’s responsibility to translate
+and swap pages as needed. Note, the value parameter is not used for this instruction, but a dummy
+value (e.g., 0) should always be provided. */
+void load (int process, char* instruction, int address, int value) 
+{ 
+	if (pageTable[process].validBit != 1) { printf("ERROR: Process [%i] not Found\n", process); return -1; } // check validity
+}
+
+/*********************************************************** MAIN ****************************************************************************/
 int main(int argc, char **argv)
 {
+	initialize(); // initilialize structs
 	char *userInput[4]; 
  	int process_id;
 	char* instruction_type;
@@ -54,7 +96,7 @@ int main(int argc, char **argv)
     	const char s[2] = ","; // token to check for
     	char *token; // token creation
 
-   		/* tokenize first input */
+   		/* tokenize first input */ 
     	printf("Instruction? ");
     	fgets(args,32,stdin); 
     	if (!token) { printf("ERROR: Invalid Input\n"); return -1; } // check for null token
@@ -78,7 +120,7 @@ int main(int argc, char **argv)
 		instruction_type = userInput[1];
 		virtual_address = atoi(userInput[2]);
 		value = atoi(userInput[3]);
-		printf("process_id: %i\n instruction_type: %s\n virtual_address: %i\n value: %i\n", process_id, instruction_type, virtual_address, value);
+		printf("process_id: %i\ninstruction_type: %s\nvirtual_address: %i\nvalue: %i\n", process_id, instruction_type, virtual_address, value);
   		masterFunction(process_id, instruction_type, virtual_address, value);	
 	}
 	return 1;
